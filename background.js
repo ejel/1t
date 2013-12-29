56395ded972239707b716304a8c764f3ae6062d0
ejel <e.jelly@gmail.com>
2013-12-28 22:42:17 -0800

    Capture tab screenshots

--- background.js
+++ background.js
@@ -1,4 +1,39 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': chrome.extension.getURL('tab.html')}, function(tab) {
+    // chrome.tabs.create({'url': chrome.extension.getURL('tab.html')}, function(tab) {
+    // });
+    // chrome.windows.getCurrent(function(win) {
+    //     chrome.tabs.captureVisibleTab(win.id, {format: "png"}, function(dataUrl) {
+    //         console.log(win);
+    //         console.log(dataUrl);
+    //     });
+    // });
+
+
+    chrome.tabs.query({currentWindow: true, active: false}, function(tabs) {
+      var populatedTabs = [];
+
+      var photoTab = function(index) {
+          chrome.tabs.update(tabs[index].id, {active: true}, function(updatedTab) {
+              chrome.tabs.captureVisibleTab({format: "png"}, function(dataUrl) {
+                  updatedTab.dataUrl = dataUrl;
+                  populatedTabs.push(updatedTab);
+
+                  var nextTabIndex = index + 1;
+                  if (nextTabIndex < tabs.length) {
+                     photoTab(nextTabIndex);
+                  } else {
+                     console.log(populatedTabs);
+                     show1Tab(populatedTabs);
+                  }
+              });
+          });
+      }
+      photoTab(0);
     });
+
+    var show1Tab = function(tabs) {
+        chrome.tabs.create({url: chrome.extension.getURL('tab.html')}, function(tab) {
+            chrome.tabs.sendMessage(tab.id, {'tabs': tabs});
+        });
+    }
 });
--- tab.js
+++ tab.js
@@ -1,5 +1,13 @@
-chrome.tabs.query({currentWindow: true, active: false}, function(tabs) {
-    for (i = 0; i < tabs.length; i++) {
-        $("body ul").append("<li>" + tabs[i].title + "</li>");
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        onPageReady(request);
     }
-});
+);
+
+var onPageReady = function(tabs) {
+    console.log(tabs);
+    for (var i = 0; i < tabs.tabs.length; i++) {
+        $('body ul').append("<img src='" + tabs.tabs[i].dataUrl + "'>");
+    }
+}
+
